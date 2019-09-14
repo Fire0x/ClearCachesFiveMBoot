b1d158b9846f28eacea6027eb9cf610edbb1e5ca
Fire0x <spocock222@yahoo.com>
2019-09-14 19:22:12 +1000

    Create Clear_Cache.bat

---
+++ Clear_Cache.bat
@@ -0,0 +1,23 @@
+@echo off
+
+
+
+del "%appdata%\..\Local\FiveM\FiveM.app\caches.xml"
+
+
+
+echo Cache.xml file is deleted
+
+
+
+cd "%appdata%\..\Local\FiveM\FiveM.app"
+
+start FiveM
+
+
+
+FiveM has started up
+
+
+
+Timeout 60[
